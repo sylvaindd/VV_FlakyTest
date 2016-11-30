2e52072788c93a3ba509cadea880f9de73fb6bb7
thelittlefireman <thomas.goureau@gmail.com>
2016-11-30 11:41:17 +0100

    add template modification

--- flakyTest/src/models/OutputPrettyViewer.java
+++ flakyTest/src/models/OutputPrettyViewer.java
@@ -1,63 +1,131 @@
 package models;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import resources.HTMLResources;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMResult;
+import javax.xml.transform.dom.DOMSource;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Stream;
 
 /**
  * Created by 15004515 on 16/11/2016.
  */
 public class OutputPrettyViewer {
-	private List<ClassWarnings>	classWarningsList;
-	private String				path;
+    private List<ClassWarnings> classWarningsList;
+    private String path;
 
-	public OutputPrettyViewer(String pathFile) {
-		this.classWarningsList = new ArrayList<>();
-		this.path = pathFile;
-	}
+    public OutputPrettyViewer(String pathFile) {
+        this.classWarningsList = new ArrayList<>();
+        this.path = pathFile;
+    }
 
-	public void addClassWarning(ClassWarnings classWarnings) {
-		classWarningsList.add(classWarnings);
-	}
+    public void addClassWarning(ClassWarnings classWarnings) {
+        classWarningsList.add(classWarnings);
+    }
 
-	private String classNameToPath(String className) {
-		return path + className.replace(".", "/") + ".java";
-	}
+    private String classNameToPath(String className) {
+        return path + className.replace(".", "/") + ".java";
+    }
 
-	public String GenerateHTMLView() {
-		String html = HTMLResources.BODY_START;
-		for (ClassWarnings classWarnings : classWarningsList) {
-			html += HTMLResources.NEW_CLASS_START + classWarnings.getClassName() + HTMLResources.NEW_CLASS_END;
-			// get le code ligne par ligne et ajouter les warnings au début des
-			// lignes correspondante
-			try (Stream<String> lines = Files.lines(Paths.get(classWarnings.getAbsolutePath()), StandardCharsets.UTF_8)) {
-				int index = 0;
-				int nbLine = 1;
-				html += HTMLResources.NEW_CODE_START;
-				for (String line : (Iterable<String>) lines::iterator) {
-					Warning actual_Warning = classWarnings.GetWarningFromLine(nbLine);
-					if (actual_Warning !=null) {
-						html += actual_Warning.GenerateHTML() + line + "\n";
-						index++;
-					}
-					else {
-						html += line + "\n";
-					}
-					nbLine++;
-				}
-				html += HTMLResources.NEW_CODE_END;
-			}
-			catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		html += HTMLResources.BODY_END;
-		return html;
-	}
+    public String GenerateHTMLView() {
+        String html = "";
+        for (ClassWarnings classWarnings : classWarningsList) {
+            html += HTMLResources.NEW_CLASS_START + classWarnings.getClassName() + HTMLResources.NEW_CLASS_END;
+            // get le code ligne par ligne et ajouter les warnings au début des
+            // lignes correspondante
+            try (Stream<String> lines = Files.lines(Paths.get(classWarnings.getAbsolutePath()), StandardCharsets.UTF_8)) {
+                int index = 0;
+                int nbLine = 1;
+                html += HTMLResources.NEW_CODE_START;
+                for (String line : (Iterable<String>) lines::iterator) {
+                    Warning actual_Warning = classWarnings.GetWarningFromLine(nbLine);
+                    if (actual_Warning != null) {
+                        html += actual_Warning.GenerateHTML() + line + "\n";
+                        index++;
+                    } else {
+                        html += line + "\n";
+                    }
+                    nbLine++;
+                }
+                html += HTMLResources.NEW_CODE_END;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        String content = null;
+        try {
+            content = new Scanner(new File(new File("").getAbsolutePath()+"/res/output_template_main.xml")).useDelimiter("\\Z").next();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return content.replace("!!!CODE!!!",html);
+    }
+
+    public void GenerateHTMLViewerTemplate() {
+
+        try {
+            File main_templateFile = new File("input.txt");
+            DocumentBuilderFactory dbFactory
+                    = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(main_templateFile);
+            doc.getDocumentElement().normalize();
+
+            File class_templateFile = new File("input.txt");
+            Document class_template = dBuilder.parse(class_templateFile);
+
+            File warnining_templateFile = new File("input.txt");
+            Document warnining_template = dBuilder.parse(warnining_templateFile);
+
+            Element warning_template = doc.getElementById("warning_template");
+
+            Document generatedDoc =  dBuilder.parse(main_templateFile);
+            Element listClasses = generatedDoc.getElementById("classList_template");
+
+            //ajouter une classe
+            Document tmpClass = copyDOC(class_template);
+            tmpClass.getElementsByTagName("i").item(0);
+           // listClasses.appendChild(tmpClass.getParentNode());
+
+
+            //ajout d'une classe
+          //  Node class_Warnings = class_template.cloneNode(true);
+            //class_Warnings.getChildNodes().item(0).getChildNodes().item(0).setNodeValue("NOM DE LA CLASSE");
+            //listClasses.appendChild(class_Warnings);
+
+            //ajout d'un warning
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Document copyDOC(Document doc){
+        try {
+            TransformerFactory tfactory = TransformerFactory.newInstance();
+            Transformer tx = tfactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            DOMResult result = new DOMResult();
+            tx.transform(source, result);
+            return (Document) result.getNode();
+        }catch (Exception e){
+            return null;
+        }
+    }
 }
--- flakyTest/src/resources/HTMLResources.java
+++ flakyTest/src/resources/HTMLResources.java
@@ -4,48 +4,6 @@ package resources;
  * Created by 15004515 on 16/11/2016.
  */
 public class HTMLResources {
-    public static final String BODY_START = "<!doctype html>\n" +
-            "<html>\n" +
-            "<head>\n" +
-            "<meta charset=\"UTF-8\">\n" +
-            "<title>output_template</title>\n" +
-            "\n" +
-            "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/css/bootstrap.min.css\" integrity=\"sha384-2hfp1SzUoho7/TsGGGDaFdsuuDL0LX2hnUp6VkX3CUQ2K4K+xjboZdsXyp4oUHZj\" crossorigin=\"anonymous\">\n" +
-            "\n" +
-            "<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js\"></script>\n" +
-            "\n" +
-            "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js\"></script>\n" +
-            "\n" +
-            "<!-- Latest compiled and minified JavaScript -->\n" +
-            "<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/js/bootstrap.min.js\" integrity=\"sha384-VjEeINv9OSwtWFLAtmc4JCtEJXXBub00gtSnszmspDLCtC0I4z4nqz7rEFbIZLLU\" crossorigin=\"anonymous\"></script>\n" +
-            "\n" +
-            "\n" +
-            "<script src=\"https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js?skin=sunburst\"></script>\n" +
-            "\n" +
-            "<script src=\"https://use.fontawesome.com/e3a541e273.js\"></script>\n" +
-            "<script>\n" +
-            "\t$(function () {\n" +
-            "\t  $('[data-toggle=\"tooltip\"]').tooltip()\n" +
-            "\t});\n" +
-            "</script>\n" +
-            "<style>\n" +
-            "\t.fa-exclamation-triangle {\n" +
-            "\t\tcolor: yellow;\n" +
-            "\t\tposition: absolute;" +
-            "\t\tleft: 3%;" +
-            "\t\tmargin-top: 3px;" +
-            "\t}\n" +
-            "    .prettyprint ol.linenums > li { list-style-type: decimal; }\n" +
-            "</style>\n" +
-            "\n" +
-            "</head>\n" +
-            "\n" +
-            "<body>\n" +
-            "\n" +
-            "<div>";
-    public static final String BODY_END = "</div>\n" +
-            "</body>\n" +
-            "</html>";
     public static final String NEW_CLASS_START = "\t<a data-toggle=\"collapse\" data-target=\"#collapseOne\" href=\"#collapseOne\">\n" +
             "    \t<h2>";
     public static final String NEW_CLASS_END = "</h2>\n" +
