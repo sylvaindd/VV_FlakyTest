cc17b90a4754978e9880b3f9731a88dde859359f
Sylvain <sylvainduarte@hotmail.fr>
2017-01-03 12:11:12 +0100

    git add .

---
+++ flakyTest/src/main/java/com/models/ClassWarnings.java
@@ -0,0 +1,55 @@
+package com.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by 15004515 on 16/11/2016.
+ */
+public class ClassWarnings {
+	private String			absolutePath;
+	private List<Warning>	warningList;
+	private String			className;
+
+	public ClassWarnings(String className, String absolutePath) {
+		this.className = className;
+		this.absolutePath = absolutePath;
+		this.warningList = new ArrayList<>();
+	}
+
+	public void addWarning(Warning warning) {
+		this.warningList.add(warning);
+	}
+
+	public List<Warning> getWarningList() {
+		return warningList;
+	}
+
+	public String getClassName() {
+		return className;
+	}
+
+	public Warning GetWarningFromLine(int line) {
+		for (Warning warning : warningList) {
+			if (warning.getLine() == line)
+				return warning;
+		}
+		return null;
+	}
+
+	public boolean isWarningLine(int line) {
+		for (Warning warning : warningList) {
+			if (warning.getLine() == line)
+				return true;
+		}
+		return false;
+	}
+
+	public String getAbsolutePath() {
+		return absolutePath;
+	}
+
+	public void setAbsolutePath(String absolutePath) {
+		this.absolutePath = absolutePath;
+	}
+}
