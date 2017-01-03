a56ba92250f41dcc17b8e173eac276304a290de4
guillaumectd <guigui.cot35@gmail.com>
2017-01-03 15:13:33 +0100

    cleaning code

--- flakyTest/src/main/java/com/models/OutputPrettyViewer.java
+++ flakyTest/src/main/java/com/models/OutputPrettyViewer.java
@@ -16,65 +16,58 @@ import java.util.stream.Stream;
  * Created by 15004515 on 16/11/2016.
  */
 public class OutputPrettyViewer {
-	private List<ClassWarnings>	classWarningsList;
-	private String				path;
+    private List<ClassWarnings> classWarningsList;
+    private String path;
 
-	public OutputPrettyViewer(String pathFile) {
-		this.classWarningsList = new ArrayList<>();
-		this.path = pathFile;
-		HTMLResources.i = 0;
-	}
+    public OutputPrettyViewer(String pathFile) {
+        this.classWarningsList = new ArrayList<>();
+        this.path = pathFile;
+        HTMLResources.i = 0;
+    }
 
-	public void addClassWarning(ClassWarnings classWarnings) {
-		classWarningsList.add(classWarnings);
-	}
+    public void addClassWarning(ClassWarnings classWarnings) {
+        classWarningsList.add(classWarnings);
+    }
 
-	private String classNameToPath(String className) {
-		return path + className.replace(".", "/") + ".java";
-	}
+    public String GenerateHTMLView() {
+        String html = "";
+        for (ClassWarnings classWarnings : classWarningsList) {
+            html += HTMLResources.getNewClassStart() + classWarnings.getClassName() + HTMLResources.getNewClassEnd();
+            // get le code ligne par ligne et ajouter les warnings au début des
+            // lignes correspondante
+            try (Stream<String> lines = Files.lines(Paths.get(classWarnings.getAbsolutePath()), StandardCharsets.UTF_8)) {
+                int index = 0;
+                int nbLine = 1;
+                html += HTMLResources.getNewCodeStart();
+                for (String line : (Iterable<String>) lines::iterator) {
+                    Warning actual_Warning = classWarnings.getWarningFromLine(nbLine);
+                    if (actual_Warning != null) {
+                        html += actual_Warning.GenerateHTML(actual_Warning.getCriticality()) + line + "\n";
+                        index++;
+                    } else {
+                        html += line + "\n";
+                    }
+                    nbLine++;
+                }
+                html += HTMLResources.getNewCodeEnd();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            HTMLResources.i++;
+        }
+        String content = null;
+        try {
+            Scanner scanner = new Scanner(new File(new File("").getAbsolutePath() + "/src/main/java/com/res/output_template_main.html"));
+            content = scanner.useDelimiter("\\Z").next();
+            scanner.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-	public String GenerateHTMLView() {
-		String html = "";
-		for (ClassWarnings classWarnings : classWarningsList) {
-			html += HTMLResources.getNewClassStart() + classWarnings.getClassName() + HTMLResources.getNewClassEnd();
-			// get le code ligne par ligne et ajouter les warnings au début des
-			// lignes correspondante
-			try (Stream<String> lines = Files.lines(Paths.get(classWarnings.getAbsolutePath()), StandardCharsets.UTF_8)) {
-				int index = 0;
-				int nbLine = 1;
-				html += HTMLResources.getNewCodeStart();
-				for (String line : (Iterable<String>) lines::iterator) {
-					Warning actual_Warning = classWarnings.getWarningFromLine(nbLine);
-					if (actual_Warning != null) {
-						html += actual_Warning.GenerateHTML(actual_Warning.getCriticalityriticality()) + line + "\n";
-						index++;
-					}
-					else {
-						html += line + "\n";
-					}
-					nbLine++;
-				}
-				html += HTMLResources.getNewCodeEnd();
-			}
-			catch (IOException e) {
-				e.printStackTrace();
-			}
-			HTMLResources.i++;
-		}
-		String content = null;
-		try {
-			Scanner scanner = new Scanner(new File(new File("").getAbsolutePath() + "/src/main/java/com/res/output_template_main.html"));
-			content = scanner.useDelimiter("\\Z").next();
-			scanner.close();
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-		}
+        return content.replace("!!!CODE!!!", html);
+    }
 
-		return content.replace("!!!CODE!!!", html);
-	}
-
-	public List<ClassWarnings> getClassWarningsList() {
-		return classWarningsList;
-	}
+    public List<ClassWarnings> getClassWarningsList() {
+        return classWarningsList;
+    }
 }
--- flakyTest/src/main/java/com/models/Warning.java
+++ flakyTest/src/main/java/com/models/Warning.java
@@ -7,62 +7,54 @@ import com.resources.HTMLResources;
  */
 public class Warning {
 
-	public enum Criticality {
-		LOW, MEDIUM, HIGH;
+    public enum Criticality {
+        LOW, MEDIUM, HIGH;
 
-		public String getColor() {
-			switch (this){
-				case LOW:
-					return "yellow";
-				case MEDIUM:
-					return "orange";
-				case HIGH:
-					return "red";
-				default:
-					return "yellow";
-			}
-		}
-	}
+        public String getColor() {
+            switch (this) {
+                case LOW:
+                    return "yellow";
+                case MEDIUM:
+                    return "orange";
+                case HIGH:
+                    return "red";
+                default:
+                    return "yellow";
+            }
+        }
+    }
 
-	private Criticality	criticalityriticality;
-	private int			line;
-	private Params		params;
+    private Criticality criticality;
+    private int line;
+    private Params params;
 
-	public Warning(Criticality criticalityriticality, Params params, int line) {
-		this.criticalityriticality = criticalityriticality;
-		this.params = params;
-		this.line = line;
-	}
+    public Warning(Criticality criticality, Params params, int line) {
+        this.criticality = criticality;
+        this.params = params;
+        this.line = line;
+    }
 
-	public String GenerateHTML(Criticality criticality) {
-		return HTMLResources.getWarningStart(criticality.getColor()) + this.getTitle() + "<br />" + this.getDescription() + HTMLResources.getWarningEnd();
-	}
+    public String GenerateHTML(Criticality criticality) {
+        return HTMLResources.getWarningStart(criticality.getColor()) + this.getTitle() + "<br />" + this.getDescription() + HTMLResources.getWarningEnd();
+    }
 
-	public Criticality getCriticalityriticality() {
-		return criticalityriticality;
-	}
+    public Criticality getCriticality() {
+        return criticality;
+    }
 
-	public void setCriticalityriticality(Criticality criticalityriticality) {
-		this.criticalityriticality = criticalityriticality;
-	}
+    public String getDescription() {
+        return params.getDescription();
+    }
 
-	public String getDescription() {
-		return params.getDescription();
-	}
+    public String getTitle() {
+        return params.getName();
+    }
 
-	public String getTitle() {
-		return params.getName();
-	}
+    public int getLine() {
+        return line;
+    }
 
-	public int getLine() {
-		return line;
-	}
-
-	public void setLine(int line) {
-		this.line = line;
-	}
-
-	public Params getParams() {
-		return params;
-	}
+    public Params getParams() {
+        return params;
+    }
 }
